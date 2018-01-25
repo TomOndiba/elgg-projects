d43dc62de0d51c77166b11b762c64b9260fcadfb
Juho Jaakkola <juho@elgg.org>
2018-01-25 20:01:18 +0100

    Calendar view legend items are now displayed on the same row

--- views/default/project/tabs/calendar.php
+++ views/default/project/tabs/calendar.php
@@ -10,7 +10,9 @@ $open_string = elgg_echo('projects:status:open');
 $closed_string = elgg_echo('projects:status:closed');
 
 echo <<<HTML
-	<p><span class="projects-status-legend open"></span>$open_string<br />
-	<span class="projects-status-legend closed"></span>$closed_string</p>
+	<p>
+		<span class="projects-status-legend open"></span>$open_string
+		<span class="projects-status-legend closed"></span>$closed_string
+	</p>
 	<div id="project-calendar" data-project_guid="{$entity->guid}"></div>
 HTML;
