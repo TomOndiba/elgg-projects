941b1a2d0a1c9721d2ae4cbb9d9e45cd845fe37d
Juho Jaakkola <juho@elgg.org>
2018-01-28 13:30:09 +0100

    Task widget now displays which project each task belongs to
    Fixes #3

--- views/default/object/task.php
+++ views/default/object/task.php
@@ -7,15 +7,25 @@ $entity = elgg_extract('entity', $vars);
 if ($full) {
 	echo elgg_view('object/task/full_view', $vars);
 } else {
+	$subtitle = '';
+	if (elgg_in_context('widgets')) {
+		// Tasks from all the different projects are displayed in the
+		// same widget, so we also need to display, which projects each
+		// task belongs to.
+		$subtitle .= $entity->getContainerEntity()->getDisplayName() . "<br />";
+	}
+
+	$subtitle .= elgg_echo('projects:deadline', [
+		date(elgg_echo('projects:date:format'), $entity->date_end),
+	]);
+
 	$params = array(
 		'entity' => $entity,
 		'title' => elgg_view('output/url', [
 			'href' => $entity->getURL(),
 			'text' => $entity->getDisplayName(),
 		]),
-		'subtitle' => elgg_echo('projects:deadline', [
-			date(elgg_echo('projects:date:format'), $entity->date_end),
-		]),
+		'subtitle' => $subtitle,
 		'metadata' => elgg_view_menu('entity', [
 			'entity' => $entity,
 			'handler' => 'task',
